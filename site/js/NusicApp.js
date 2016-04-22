a6662cd7780661cac94d2cb095e7b2bb7af07338
Sandip Chitale <sandipchitale@gmail.com>
2016-04-22 10:05:29 -0700

    Support going to latest

--- site/js/NusicApp.js
+++ site/js/NusicApp.js
@@ -35,6 +35,44 @@
             // Do not use in new projects.
             $sceProvider.enabled(false);
           }])
+          .run(['$window', '$log','$http', '$q', function($window, $log, $http, $q) {
+            var url = $window.location.href, urlParts, prefix, major, minor, suffix, foundNext = false;
+            function findLatest(prefix, major, minor, suffix, foundNext) {
+              var deferred = $q.defer();
+              $http.head(prefix+major+'.'+(minor+1)+suffix).success(function() {
+                foundNext = true;
+                findLatest(prefix, major, minor + 1, suffix, foundNext).then(function(url){
+                  $log.log('Found ' + url);
+                  deferred.resolve(url);
+                },function() {
+                  if (foundNext) {
+                    deferred.resolve(url);
+                  } else {
+                    deferred.reject()
+                  }
+                });
+              }).error(function(error, status) {
+                if (foundNext) {
+                  $window.location.href = prefix+major+'.'+minor+suffix;
+                }
+              });
+              return deferred.promise;
+            }
+            if (angular.isString(url)) {
+              urlParts = url.match(/(https?:\/\/cdn.rawgit.com\/sandipchitale\/nusic\/)(\d+)\.(\d+)(\/.*)/);
+              if (angular.isArray(urlParts)) {
+                if (urlParts.length >= 5) {
+                  prefix = urlParts[1];
+                  major = urlParts[2];
+                  minor = parseInt(urlParts[3]);
+                  suffix = urlParts[4];
+                  findLatest(prefix, major, minor, suffix, false).then(function(url) {
+                    $window.location.href = url;
+                  });
+                }
+              }
+            }
+          }])
           .controller('NusicController', ['$modal', '$window', function($modal, $window) {
               var vm = this;
               vm.showVideo = function(videoHref) {
